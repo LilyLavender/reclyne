b09829128f050a9a1fb219806462aa2860773aa7
LilyLavender <lilylambda136@gmail.com>
2024-04-03 16:02:37 -0500

    a[1.0.9] Improved functionality of theme changer • Light & dark colors are now calculated based on accent color. This will change in the future so that light, dark, accent, and text colors can all be changed separately • Removed hardcoded colors from css :root • Added jQuery xcolor to help with all of the color math • These changes have increased site lag. Optimizations to these featues will come before the end of the alpha

--- js/theme.js
+++ js/theme.js
@@ -1,3 +1,7 @@
+const root = $(':root');
+// Dark, light, accent
+var allColors = ["#110b1d", "#d5d9f2", "#9da8ec"];
+
 // WIP
 function themeChanger() {
     let c = document.createElement("input");
@@ -8,8 +12,36 @@ function themeChanger() {
         // Get color from picker
         pickedColor = this.value;
 
-        // Set accent color to picked color
-        document.documentElement.style.setProperty('--accent', pickedColor);
+        // Calculate light and dark colors from accent (DEBUG ONLY, TODO)
+        allColors[0] = $.xcolor.opacity(pickedColor, '#101010', 0.90);
+        allColors[1] = $.xcolor.opacity(pickedColor, '#F0F0F0', 0.90);
+
+        // Set colors
+        allColors[2] = pickedColor;
+        calculateColors();
     });
     c.click();
+}
+
+// Calculate colors and set them in :root
+function calculateColors() {
+    // Get dark, light, and accent colors
+    var colorDark = allColors[0];
+    var colorLight = allColors[1];
+    var colorAccent = allColors[2];
+
+    // Set dark, light, and accent colors
+    root.css('--dark', colorDark);
+    root.css('--light', colorLight);
+    root.css('--accent', colorAccent);
+
+    // Get middle colors between light and accent
+    var colorAccent50 = $.xcolor.average(colorLight, colorAccent).getRGB(); // xcolor.average returns an xcolor object, but cannot take one
+    var colorAccent25 = $.xcolor.average(colorAccent50, colorLight);
+    var colorAccent75 = $.xcolor.average(colorAccent50, colorAccent);
+
+    // Set middle colors
+    root.css('--accent-25', colorAccent25);
+    root.css('--accent-50', colorAccent50);
+    root.css('--accent-75', colorAccent75);
 }
